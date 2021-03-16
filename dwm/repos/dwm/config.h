1073b193892d8b9117c71283c742129dba6d898e
Kyle Pollock <kyle.pollock7@gmail.com>
2021-03-15 22:26:09 -0500

    Go back to st from alacritty

--- dwm/repos/dwm/config.h
+++ dwm/repos/dwm/config.h
@@ -58,7 +58,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray2, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static const char *brightdown[] = { "xbacklight", "-dec", "5", NULL };
 static const char *brightup[]   = { "xbacklight", "-inc", "5", NULL };
